1bb87f16349aa327a7f5012bf7e3bf00ff61b873
Ahmed Shehata <68691316+youngshehata@users.noreply.github.com>
2025-08-13 15:25:38 +0300

    feat: basic configrations

--- apps/auth/test/app.e2e-spec.ts
+++ apps/auth/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AuthModule } from './../src/auth.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AuthModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
